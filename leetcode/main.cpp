6aedd873315f2dd6e3b714c5142ac2cad0aa6cf6
Iven2166 <iven2166@163.com>
2022-12-19 19:00:34 +0800

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by 刘桂洲 on 2022/12/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
